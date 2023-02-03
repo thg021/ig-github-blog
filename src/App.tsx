034875f1b77aed74f15678ed1a1028e15a5eed25
Thiago Silva <itm_tsilva@uolinc.com>
2023-02-03 19:44:03 -0300

    feat: adicionado estilização inicial do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/theme/default'
+
 function App() {
-  return <div>Thiago BOra começar a loucura</div>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <div>Thiago BOra começar a loucura</div>
+    </ThemeProvider>
+  )
 }
 
 export default App
